8f8721a91f486b1354991bc96f11684ede93c4c2
Ben Hall <benhall.ninetysix@gmail.com>
2021-11-22 12:07:52 +0000

    Switch functionality added

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,29 @@
 "use strict";
 
-const onAccTitleClick = (e) => {
-    console.log('clicked');
-    e.target.parentElement.classList.toggle('active');
+const resetAccordionItems = () => {
+    let activeItems = document.querySelectorAll('.accordion__item.active');
+    activeItems.forEach(function(item) {
+        item.classList.remove('active');
+    });
+};
+
+const toggleCb = document.getElementById('method_toggle');
+
+toggleCb.addEventListener('click', () => {
+    if (toggleCb.checked) {
+        resetAccordionItems();
+    }
+}, false);
+
+const onAccTitleClick = (e) => { 
+    if (toggleCb.checked) {
+        if(!e.target.classList.contains('active')){
+            resetAccordionItems();
+            e.target.parentElement.classList.add('active');
+        }
+    } else {
+        e.target.parentElement.classList.toggle('active');
+    }
 }
   
 const accTitles = document.querySelectorAll('.accordion__item__title');
